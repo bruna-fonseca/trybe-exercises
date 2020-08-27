ed5524c70c3d2b9740add853bd16283dfd7912c0
Bruna Fonsêca <1998bfonseca@gmail.com>
2020-08-27 20:14:50 -0300

    exercicios do 4.1

---
+++ bloco_4/dia_1/bigger-between-2-numbers.js
@@ -0,0 +1,9 @@
+let a = 3;
+let b = 5;
+
+if (a > b) {
+    console.log(a + " é maior que " + b);
+}
+else {
+    console.log(b + " é maior que " + a);
+}
---
+++ bloco_4/dia_1/return-bigger-number.js
@@ -0,0 +1,9 @@
+let a = 20;
+let b = 10;
+
+if (a > b) {
+    return a 
+}
+else {
+    
+}
