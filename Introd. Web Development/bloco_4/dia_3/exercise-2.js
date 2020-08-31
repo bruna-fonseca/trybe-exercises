d0effdc7f31a430813ce775c0a0914b50ea3ef3a
Bruna Fonsêca <1998bfonseca@gmail.com>
2020-08-31 19:33:44 -0300

    primeiros exercicios de 4.3

---
+++ Introd. Web Development/bloco_4/dia_3/exercise-2.js
@@ -0,0 +1,12 @@
+// Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo 
+// retângulo com 5 asteriscos de base.
+
+let n = 5;
+let asterisco = "*";
+let linha;
+let input = "";
+
+for (linha = 0; linha < n; linha++) {
+  input = input + asterisco;
+  console.log(input);
+}
