32e662f3894ecd9a940b0900568aa8be2539edb1
Bruna Fonsêca <1998bfonseca@gmail.com>
2020-09-17 18:48:53 -0300

    html forms exercise

---
+++ Fundamentals of Web Development/block_6/day_1/script.js
@@ -0,0 +1,9 @@
+// const estados = ["ac", "al", "am", "ap", "ba", "ce", "df", "es", "go", "ma", "mt", "ms", "mg", "pa", "pb", "pr", "pe", "pi", "rj", "rn", "ro", "rs", "rr", "sc", "se", "sp", "to"];
+// const select = document.getElementById('estados');
+
+// select.addEventListener('click', function () {
+//     for (let i = 0; i < estados.length; i +=1) {
+//         optionsEl = document.createElement('option');
+//         select.appendChild(optionsEl);
+//     }
+// })
